207dea01c4c39e494afd40257bedeaa807ce1efe
novneetnov <novneet346iith@gmail.com>
2016-01-29 17:28:27 +0530

    Add Spinner and add ajax:send event handlers to stocks.js

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,16 +1,25 @@
 var init_stock_lookup = function() {
+
+	var form_div = $('#stock-lookup-form');
+
+	form_div.on('ajax:send', function(event, xhr){
+		show_spinner();
+	});
+
 	// How is the 'data' the entire _lookup.html.erb ?
-	$('#stock-lookup-form').on('ajax:success', function(event, data, status) {
+	form_div.on('ajax:success', function(event, data, status) {
+		hide_spinner();
 		$('#stock-lookup').replaceWith(data);
 		init_stock_lookup();
 	});
 
-	$('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error) {
-		alert("hello");
+	form_div.on('ajax:error', function(event, xhr, status, error) {
+		hide_spinner();
 		$('#stock-lookup-results').replaceWith('');
 		$('#stock-lookup-errors').replaceWith('Stock was not found');
 		init_stock_lookup();
 	});
+
 };
 
 $(document).ready(function() {
