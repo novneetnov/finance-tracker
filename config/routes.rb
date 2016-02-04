83f88aa7e46442a0503d992b300a1e891ad3ca75
novneetnov <novneet346iith@gmail.com>
2016-02-04 17:04:15 +0530

    Add my_friends views and friendship routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {registrations: "user/registrations"}
   resources :user_stocks, except: [:show, :edit, :update]  # why index action is not excluded?
+	resources :users, only: [:show]
+	resources :friendships, only: [:create, :destroy]
 	root 'welcome#index'
 	get 'my_portfolio', to: 'users#my_portfolio'
 	get 'search_stocks', to: 'stocks#search'
